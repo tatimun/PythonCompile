f272cc52a1027c6fa8199189a45c19399deaf06f
Tita Mun <136043018+tatimun@users.noreply.github.com>
2024-11-20 11:07:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola, Mundo! Esta es una aplicación de prueba desplegada en Docker y ECR."
+    # Obtén la clave de OpenAI desde la variable de entorno
+    openai_api_key = os.getenv('OPENAI_API_KEY', 'Clave no definida')
+    return (
+        "¡Hola, Mundo! Esta es una aplicación de prueba desplegada en Docker y ECR.<br>"
+        f"Clave de OpenAI: {openai_api_key}"
+    )
 
 if __name__ == '__main__':
     # Obtén el puerto desde la variable de entorno o usa 5000 por defecto
